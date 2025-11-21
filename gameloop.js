ec64924c84b006c65b840a960caa7bdf31976766
Alex-Schneider93 <schneider.alex93@icloud.com>
2025-11-21 20:03:37 +0100

    Update gameloop.js

--- gameloop.js
+++ gameloop.js
@@ -1,43 +1,8 @@
-let GAMELOOPJS_SPEED = 1000 / 60;
-let GAMELOOPJS_SPACE_TIMEOUT = 100;
-let GAMELOOPJS_INTERVALS = [];
+let KEYS = {};
 
-const GAMELOOPJS_KEY = {};
+document.addEventListener("keydown", e => KEYS[e.keyCode] = true);
+document.addEventListener("keyup",   e => KEYS[e.keyCode] = false);
 
-document.addEventListener('keydown', (e) => GAMELOOPJS_KEY[e.keyCode] = true);
-document.addEventListener('keyup',   (e) => GAMELOOPJS_KEY[e.keyCode] = false);
-
-function leftKeyPressed() {}
-function rightKeyPressed() {}
-function upKeyPressed() {}
-function downKeyPressed() {}
-function spaceKeyPressed() {}
-
-function gameInterval(fn, time) {
-    let i = setInterval(fn, time);
-    GAMELOOPJS_INTERVALS.push(i);
-    return i;
-}
-
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-GAMELOOPJS_START();
-function GAMELOOPJS_START() {
-    let spaceLock = false;
-
-    gameInterval(() => {
-        if (GAMELOOPJS_KEY[37]) leftKeyPressed();
-        if (GAMELOOPJS_KEY[39]) rightKeyPressed();
-        if (GAMELOOPJS_KEY[38]) upKeyPressed();
-        if (GAMELOOPJS_KEY[40]) downKeyPressed();
-        if (GAMELOOPJS_KEY[32]) {
-            if (!spaceLock) {
-                spaceKeyPressed();
-                spaceLock = true;
-                setTimeout(() => spaceLock = false, GAMELOOPJS_SPACE_TIMEOUT);
-            }
-        }
-    }, GAMELOOPJS_SPEED);
+function gameLoop(callback) {
+    return setInterval(callback, 16); // ≈60 FPS
 }
